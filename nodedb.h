2dd861fdb4f010d72f641e3b48245a90432f1481
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-12-18 10:29:58 +0000

    Whitspace, added get_name()
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4153 49c17534-1d02-dc11-b283-0030488c597c

--- nodedb.h
+++ nodedb.h
@@ -6,8 +6,9 @@
 
 typedef struct Node	Node;
 
-extern void	nodedb_init(void);
+extern void		nodedb_init(void);
 
-extern Node *	nodedb_new(VNodeID node_id);
-extern void	nodedb_set_name(Node *node, const char *name);
-extern Node *	nodedb_lookup(VNodeID node_id);
+extern Node *		nodedb_new(VNodeID node_id);
+extern void		nodedb_set_name(Node *node, const char *name);
+extern const char *	nodedb_get_name(const Node *node);
+extern Node *		nodedb_lookup(VNodeID node_id);
