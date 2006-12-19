370ea701cbfb0c171bdc833954a3d5d2c389b331
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-12-19 10:04:40 +0000

    Added size() and hear()
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4164 49c17534-1d02-dc11-b283-0030488c597c

--- channel.h
+++ channel.h
@@ -10,3 +10,6 @@ extern Channel *	channel_new(const char *name);
 extern Channel *	channel_lookup(const char *name);
 extern int		channel_user_add(Channel *channel, User *user);
 extern int		channel_user_remove(Channel *channel, User *user);
+extern size_t		channel_size(const Channel *channel);
+
+extern void		channel_hear(Channel *channel, const User *speaker, const char *text);
