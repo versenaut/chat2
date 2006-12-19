d17a1b4a4d240e37e4dfa7ac2e26e5a9a2eb07f2
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-12-19 10:04:29 +0000

    Implemented user_add() and remove(); added hear()
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4163 49c17534-1d02-dc11-b283-0030488c597c

--- channel.c
+++ channel.c
@@ -9,8 +9,13 @@
 #include "channel.h"
 #include "qsarr.h"
 
+#if defined _WIN32
+#define	snprintf	_snprintf
+#endif
+
 struct Channel {
 	char	name[64];
+	QSArr	*members;	
 };
 
 static struct {
@@ -38,15 +43,27 @@ void channel_init(void)
 	ChannelInfo.channels = qsarr_new(cmp_channel_sort, cmp_channel_key);
 }
 
+static int cmp_user_sort(const void **a, const void **b)
+{
+	const User	*ma = *(User **) a, *mb = *(User **) b;
+
+	return ma < mb ? -1 : ma > mb;
+}
+
+static int cmp_user_key(const void *a, const void *key)
+{
+	return a < key ? -1 : a > key;
+}
+
 Channel * channel_new(const char *name)
 {
 	Channel	*ch;
 
 	if((ch = malloc(sizeof *ch)) != NULL)
 	{
-		strcpy(ch->name, name);
-
+		snprintf(ch->name, sizeof ch->name, "%s", name);
 		qsarr_insert(ChannelInfo.channels, ch);
+		ch->members = qsarr_new(cmp_user_sort, cmp_user_key);
 	}
 	return ch;
 }
@@ -58,14 +75,41 @@ Channel * channel_lookup(const char *name)
 
 int channel_user_add(Channel *channel, User *user)
 {
+	char	buf[128];
+
+	if(channel == NULL || user == NULL)
+		return 0;
+	if(qsarr_lookup(channel->members, user) != NULL)
+		return 0;
+	qsarr_insert(channel->members, user);
+	/* Announce the new user. */
+	snprintf(buf, sizeof buf, "%s has joined channel \"%s\"\n", user_get_name(user), channel->name);
+	channel_hear(channel, NULL, buf);
+	return 1;
 }
 
 int channel_user_remove(Channel *channel, User *user)
 {
+	if(channel == NULL || user == NULL)
+		return 0;
+	qsarr_remove(channel->members, user);
+	return 1;
 }
 
-void channel_hear(Channel *channel, const char *sender, const char *text)
+size_t channel_size(const Channel *channel)
 {
+	return channel != NULL ? qsarr_size(channel->members) : 0;
+}
+
+void channel_hear(Channel *channel, const User *speaker, const char *text)
+{
+	const char	*spk;
+	int		i;
+	User		*u;
+
 	if(channel == NULL || text == NULL)
 		return;
+	spk = speaker != NULL ? user_get_name(speaker) : "<server>";
+	for(i = 0; (u = qsarr_index(channel->members, i)) != NULL; i++)
+		user_hear(u, channel->name, spk, text);
 }
