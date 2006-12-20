aba4bc8e43f6b56a95346ae5ada78fa3f044e38f
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-12-20 08:20:04 +0000

    Bullet-proofing; notifies channels
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4185 49c17534-1d02-dc11-b283-0030488c597c

--- cmd_nick.c
+++ cmd_nick.c
@@ -7,8 +7,29 @@
 
 #include "cmd_nick.h"
 
+#if defined _WIN32
+#define	snprintf	_snprintf
+#endif
+
 int cmd_nick(Channel *channel, User *speaker, const char *text)
 {
+	int	i;
+	Channel	*ch;
+	char	buf[256];
+
+	if(strncmp(text, "<server>", 8) == 0)	/* Disallow the reserved server name. */
+		return 0;
+
+	if(user_lookup(text) != NULL)		/* Refuse if nick is in use. */
+		return 0;
+
+	/* Before doing the change, go through all channels and notify. */
+	snprintf(buf, sizeof buf, "%s is now known as %s\n", user_get_name(speaker), text);
+	for(i = 0; (ch = channel_index(i)) != NULL; i++)
+	{
+		if(channel_user_is_member(ch, speaker))
+			channel_hear(ch, NULL, buf);
+	}
 	user_set_name(speaker, text);
 	return 1;
 }
