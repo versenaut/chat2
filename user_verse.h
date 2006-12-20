8dab2c820c33f715aed0ff6b09da63ef4bdfe260
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-12-20 08:23:56 +0000

    Renamed, replaced - with _
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4186 49c17534-1d02-dc11-b283-0030488c597c

---
+++ user_verse.h
@@ -0,0 +1,19 @@
+/*
+ * 
+*/
+
+#include "verse.h"
+
+#include "nodedb.h"
+#include "user.h"
+
+typedef struct {
+	User	user;
+	uint16	group_id;
+	uint16	method_id;
+	Node	*node;
+} UserClient;
+
+extern User *	user_verse_new(const char *name, VNodeID node_id, uint16 group_id, uint16 method_id);
+
+extern User *	user_verse_from_node_id(VNodeID sender);
