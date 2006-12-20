3c3d5f13780a1c3a2084c4b57e0ac58279b7a980
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-12-20 13:17:15 +0000

    Silence
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4208 49c17534-1d02-dc11-b283-0030488c597c

--- cmd_chanop.c
+++ cmd_chanop.c
@@ -32,10 +32,7 @@ int cmd_leave(Channel *channel, User *speaker, const char *text)
 			return 1;
 		channel_user_remove(ch, speaker);
 		if(channel_size(ch) == 0)
-		{
-			printf("destroying channel %s, it's empty\n", text);
 			channel_destroy(ch);
-		}
 		return 1;
 	}
 	return 0;
