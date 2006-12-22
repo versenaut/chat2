85949733c7c782b0a37fc6af6a52b3c238addc71
Emil Brisk <emil.brink@interactiveinstitute.se>
2006-12-22 08:17:58 +0000

    Simple command to echo the server's version
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4221 49c17534-1d02-dc11-b283-0030488c597c

---
+++ cmd_version.c
@@ -0,0 +1,20 @@
+/*
+ * 
+*/
+
+#include "chat2.h"
+
+#include "channel.h"
+#include "user.h"
+
+#include "cmd_version.h"
+
+int cmd_version(Channel *channel, User *speaker, const char *text)
+{
+	char	buf[256];
+
+	snprintf(buf, sizeof buf, "%s", VERSION);
+	user_hear(speaker, "", NULL, buf);
+
+	return 1;
+}
---
+++ cmd_version.h
@@ -0,0 +1,5 @@
+/*
+ * 
+*/
+
+extern int	cmd_version(Channel *channel, User *speaker, const char *text);
